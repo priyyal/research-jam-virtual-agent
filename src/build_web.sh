89f1175b81c8b09cbefd71e3c6f26fa0afd8d57c
Priyal Duggal <priyallduggal.23@gmail.com>
2026-03-28 20:52:08 -0300

    Publish updated loading screen

--- src/build_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf build/web build/web-cache
-pygbag .
-perl -pi -e 's|https?://[^"]*browserfs\.min\.js|https://cdnjs.cloudflare.com/ajax/libs/BrowserFS/2.0.0/browserfs.min.js|g' build/web/index.html
-echo "Build complete. Now run:"
-echo "cd build/web && python -m http.server 8000"
